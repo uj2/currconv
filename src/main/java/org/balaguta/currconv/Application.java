cc9a5818e370e1ad6bbba6cb79f4081741dcc0c3
Andrii Balahuta <andrey.balaguta@gmail.com>
2017-01-27 12:09:17 -0500

    added basic spring security support with login form

--- src/main/java/org/balaguta/currconv/Application.java
+++ src/main/java/org/balaguta/currconv/Application.java
@@ -1,7 +1,12 @@
 package org.balaguta.currconv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,10 +16,40 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class Application extends WebMvcConfigurerAdapter {
 
 
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("joe@example.com").password("joe123").roles("USER");
+    }
+
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/convert").setViewName("convert");
     }
+
+    @Configuration
+    public static class AppAccessConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                        .antMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                        .loginPage("/login")
+                            .permitAll()
+                    .and()
+                    .logout()
+                        .permitAll();
+        }
+
+    }
+
 
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
