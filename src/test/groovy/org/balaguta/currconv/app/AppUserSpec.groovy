97151ff2bdbd0fa9a4c5afc21e3c1ee0115f9bc8
Andrii Balahuta <andrey.balaguta@gmail.com>
2017-01-27 20:28:01 -0500

    added spock support (a test fails to test ci)

---
+++ src/test/groovy/org/balaguta/currconv/app/AppUserSpec.groovy
@@ -0,0 +1,21 @@
+package org.balaguta.currconv.app
+
+import org.balaguta.currconv.data.entity.User
+import spock.lang.Specification
+
+class AppUserSpec extends Specification {
+
+    def "user proxy methods"() {
+        def userEntity = new User()
+        userEntity.firstName = "Joe"
+        userEntity.lastName = "Sample"
+
+        when:
+        def user = new AppUser("joe@example.com", "", userEntity, Collections.emptyList())
+
+        then:
+        user.firstName != "Joe"
+        user.lastName == "Sample"
+        user.fullName == "Joe Sample"
+    }
+}
